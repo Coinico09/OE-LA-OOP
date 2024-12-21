47b20affd926423a46bfe79f31541fd2229c48eb
jodel <jodelnicolo.garcia@fcpc.edu.ph>
2024-12-21 20:53:37 +0800

    Create LA27main.py

---
+++ LA/27/LA27main.py
@@ -0,0 +1,15 @@
+import turtle as main
+
+Leonardo = main.Leonardo("Leonardo", "L")
+Michaelangelo = main.Michaelangelo("Michaelangelo", "M")
+Donatello = main.Donatello("Donatello", "D")
+Raphael = main.Raphael("Raphael", "R")
+
+print(Leonardo.alias)
+print(Michaelangelo.alias)
+print(Donatello.alias)
+print(Raphael.alias)
+
+if __name__ == "__main__":
+    Leonardo = main.Leonardo("Leonardo Decaprio", "Leo")
+    print(Leonardo.alias)
