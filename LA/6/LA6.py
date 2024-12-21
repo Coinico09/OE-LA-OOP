f89a3c36bae9f65d85348004af4ad24c4b764d85
jodel <jodelnicolo.garcia@fcpc.edu.ph>
2024-12-21 20:16:19 +0800

    Create LA6.py

---
+++ LA/6/LA6.py
@@ -0,0 +1,11 @@
+class laptop:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model 
+    
+
+    def laptop_info(self):
+        pass
+
+laptop1 = laptop("razer", "predator")
+print(laptop1.laptop_info())
