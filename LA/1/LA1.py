b47d6dd4e08aaf4b79d958b511a2edc6218b2c6f
jodel <jodelnicolo.garcia@fcpc.edu.ph>
2024-12-21 20:13:02 +0800

    Create LA1.py

---
+++ LA/1/LA1.py
@@ -0,0 +1,15 @@
+class hero():
+    name = "miya"
+    role = "marksman"
+    hp = "hp: 2225"
+    mana = "mana: 445"
+    nomal_attck = "normal attck: 115"
+
+
+marksman = hero()
+print(f'''
+{hero.name}
+{hero.role}
+{hero.hp}
+{hero.mana}
+{hero.nomal_attck}''')
