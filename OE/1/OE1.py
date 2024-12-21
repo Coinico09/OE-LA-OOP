38de395c4cc4a5d7865206d4a43815f243e6e0df
jodel <jodelnicolo.garcia@fcpc.edu.ph>
2024-12-21 20:57:34 +0800

    Create OE1.py

---
+++ OE/1/OE1.py
@@ -0,0 +1,80 @@
+class hero():
+    def __init__(self, name, role, dmg_type, health, auto_attck_dmg, mana):
+         self.name = name
+         self.role = role
+         self.dmg_type = dmg_type
+         self.health = health
+         self.mana = mana
+         self.auto_attck_dmg = auto_attck_dmg
+         
+         
+    def __str__(self):
+        return f"this hero is {self.dmg_type}."
+        
+    def describe(self):
+        print (f"{self.name} is a {self.role} with a {self.dmg_type} power")
+        
+    def meta(self):
+        return f"{self.name} is a meta in this season."
+        
+        
+hero_mm1 = hero("claude", "marksman", "burst damage",2370,97,450)
+hero_fighter1 = hero("paquito", "fighter", "close damage",2798,121,0)
+hero_support1 = hero("rafaela", "support", "heal",2441,177,545,)
+hero_tank1 = hero("minotaur", "aoe", "sustain",2859,123,0)
+hero_support1 = hero("floryn", "debuff", "heal",2401,119,550)
+
+print(f'''
+{hero_mm1.name}
+{hero_mm1.role}
+{hero_mm1.health} hp
+{hero_mm1.mana} mana
+{hero_mm1.auto_attck_dmg} basic damage
+{hero_mm1.dmg_type}''')
+hero_mm1.describe()
+print(hero_mm1)
+print(hero_mm1.meta())
+
+print(f'''
+{hero_fighter1.name}
+{hero_fighter1.role}
+{hero_fighter1.health} hp
+{hero_fighter1.mana} mana
+{hero_fighter1.auto_attck_dmg} basic damage
+{hero_fighter1.dmg_type}''')
+hero_fighter1.describe()
+print(hero_fighter1)
+print(hero_fighter1.meta())
+
+print(f'''
+{hero_support1.name}
+{hero_support1.role}
+{hero_support1.health} hp
+{hero_support1.mana} mana
+{hero_support1.auto_attck_dmg} basic damage
+{hero_support1.dmg_type}''')
+hero_support1.describe()
+print(hero_support1)
+print(hero_support1.meta())
+
+print(f'''
+{hero_tank1.name}
+{hero_tank1.role}
+{hero_tank1.health} hp
+{hero_tank1.mana} mana
+{hero_tank1.auto_attck_dmg} basic damage
+{hero_tank1.dmg_type}''')
+hero_tank1.describe()
+print(hero_tank1)
+print(hero_tank1.meta())
+
+print(f'''
+{hero_fighter1.name}
+{hero_fighter1.role}
+{hero_fighter1.health} hp
+{hero_fighter1.mana} mana
+{hero_fighter1.auto_attck_dmg} basic damage
+{hero_fighter1.dmg_type}''')
+hero_fighter1.describe()
+print(hero_fighter1)
+print(hero_fighter1.meta())
